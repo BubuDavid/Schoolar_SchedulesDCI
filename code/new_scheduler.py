e36555aaa01921b6aea6a5d0beec5954344e1466
BubuDavid <david.pedroza.segoviano@gmail.com>
2020-12-19 22:13:11 -0600

    Updating methods

--- code/new_scheduler.py
+++ code/new_scheduler.py
@@ -3,15 +3,13 @@ import requests
 import lxml.html as lh
 from bs4 import BeautifulSoup
 import pandas as pd
-import prettify
+
 
 url='http://www.dci.ugto.mx/estudiantes/index.php/mcursos/horarios-licenciatura'
 #Create a handle, page, to handle the contents of the website
 page = requests.get(url)
 # Creating a beautiful soup so we can read it as html
 soup = BeautifulSoup(page.content, 'html.parser')
-# print the code
-#print(soup.prettify())
 
 # Finding the table
 tbs = soup.findAll('table')[1]
@@ -40,8 +38,7 @@ for index, td in enumerate(tds[7:]):
 
 df = pd.DataFrame(rows, columns=columns) 
 df = df.drop(columns = ["NO."])
-#df.to_excel ("./Todas_Materias.xls", index = True, header=True)
-
+df.index = list(range(1,len(df)+1))
 
 # User thingy
 subjects = []
@@ -49,19 +46,12 @@ user = input("Ingresa tu nombre por favor: ")
 subject_file = open(f"./Users/{user}.txt", "r",  encoding='utf-8')
 for subject in subject_file:
     subjects.append(subject[:-1])
+    
+subject_file.close()
 
-# My schedule:
-# subjects = [
-#     "Ecuaciones Diferenciales Parciales",
-#     "Electromagnetismo",
-#     "Mecánica Analítica",
-#     "Termodinámica",
-#     "Física Cuántica"
-# ]
-# Uppering
 subjects = [subject.upper() for subject in subjects]
 all_subjects = []
-print(subjects)
+
 
 # Aislating my subjects
 for subject in subjects:
@@ -71,7 +61,6 @@ for subject in subjects:
 s_index = []
 for subject in all_subjects:
     s_index.append(list(subject.index))
-s_index
 
 # I'll create all the combinations and with a function 
 # I'll check if that schedule is posible. 
@@ -82,54 +71,41 @@ for i in range(len(schedules_index)):
     for j in range(i+1, len(schedules_index)):
         if sorted(list(schedules_index[i])) == sorted(list(schedules_index[j])):
             print("Diablos", i, j)
-print(len(schedules_index))
 
 # Creando todos los horarios posibles
-# Making all the schedules possibles
+# Making all the possible schedules
 schedules = []
 for i, indexes in enumerate(schedules_index):
     schedules.append(pd.DataFrame(columns=columns[1:]))
     for index in indexes:
-        schedules[i] = schedules[i].append(df.iloc[index])
+        schedules[i] = schedules[i].append(df.loc[index])
         schedules[i] = schedules[i].sort_index()
 
-# How many schedules we generate?
-print("Total generated", len(schedules))
-# This code tell us if there is a schedule repeated.
-# But we checked before so it is just to prove we are doing it okay 
-for i in range(len(schedules)):
-    for j in range(i+1, len(schedules)):
-        if list(schedules[i].index) == list(schedules[j].index):
-            print("Diablos", i, j)
 
 # Function
 def isPossible(dia1, dia2):
-    if dia1 == "\xa0" or dia2 == "\xa0" or dia1 == "1 HORA EN LÌNEA" or dia2 == "1 HORA EN LÌNEA":
+    if dia1 == "\xa0" or dia2 == "\xa0" or dia1 == "1 HORA EN LÌNEA" or dia2 == "1 HORA EN LÌNEA" or dia1 == "PENDIENTE" or dia2 == "PENDIENTE":
         return True
-    if dia1 == "PENDIENTE" or dia2 == "PENDIENTE":
+    if dia1 == "" or dia2 == "":
         return True
     dia1 = dia1.split("/")
     dia2 = dia2.split("/")    
     
-    day1 = dia1[0]
-    day2 = dia2[0]
-    hour1 = dia1[1]
-    if len(dia2) > 1:
-        hour2 = dia2[1]
-    else:
-        hour2 = ""
     
-    hour1 = list(range(int(hour1.split("-")[0]), int(hour1.split("-")[1])))
-    hour2 = list(range(int(hour2.split("-")[0]), int(hour2.split("-")[1]))) 
+    d_semana1 = dia1[0]
+    d_semana2 = dia2[0]
+    hora1 = dia1[1]
+    hora2 = dia2[1]
+      
     
-    if day1 == day2:
-        for h in hour1:
-            if h in hour2:
+    hora1 = list(range(int(hora1.split("-")[0]), int(hora1.split("-")[1])))
+    hora2 = list(range(int(hora2.split("-")[0]), int(hora2.split("-")[1]))) 
+    
+    if d_semana1 == d_semana2:
+        for h in hora1:
+            if h in hora2:
                 return False
     return True
-    
-    #print(day1, hour1)
-    #print(day2, hour2)    
 
 # Filter the schedules that are possible.
 possibles_schedules = []
@@ -142,23 +118,21 @@ for i, schedule in enumerate(schedules):
                 possible = False
     if possible == True:
         possibles_schedules.append(schedule)
+            
+print("Hay " + str(len(possibles_schedules)) + " posibles horarios")
 
-print("Possibles schedules:", len(possibles_schedules))
 
-# Creating excel
+
 # Function to insert a day in the calendar
 def insert_day(calendy, s, j, subject):
     m = list(s[f"DÍA/HORA/AULA{j}"])[0].split("/")
     day = m[0]
-    if day == "\xa0" or day == "1 HORA EN LÌNEA" or day == "PENDIENTE":
+    if day == "\xa0" or day == "":
         return
     hour = m[1]
     place = m[2]
     hour = list(range(int(hour.split("-")[0]), int(hour.split("-")[1])))
     hour = [f"{h}:00 hrs" for h in hour]
-    #print(day)
-    #print(hour)
-    #print(place)
 
     for h in hour:
         if ("LAB" in place or "PENDIENTE" in place) and not "LAB" in subject:
@@ -167,11 +141,10 @@ def insert_day(calendy, s, j, subject):
             subject = "EDP"
         
         calendy[day][h] = subject
-            
-    # Styling the cells
+
 
 # Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter(f'./Users/{user}.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter(f'Horarios/{user}.xlsx', engine='xlsxwriter')
 
 for i, schedule in enumerate(possibles_schedules):
     # Create a DataFrame with the format of a schedule
@@ -184,8 +157,6 @@ for i, schedule in enumerate(possibles_schedules):
         s = schedule[schedule["UNIDAD DE APRENDIZAJE"] == subject]
         for j in range(1,4):
             insert_day(calendy, s, j, subject)
-    # Styling
-    #calendy.style.apply(style_function)
     
     # Adding description
     dicc = {}
@@ -198,11 +169,11 @@ for i, schedule in enumerate(possibles_schedules):
     
     # At this point, the schedule is created, but we need to save it in a sheet.
     # Convert the dataframe to an XlsxWriter Excel object.
-    calendy.to_excel(writer, sheet_name=f'Sheet{i}')
+    calendy.to_excel(writer, sheet_name=f'Horario {i+1}')
 
     # Get the xlsxwriter workbook and worksheet objects.
     workbook  = writer.book
-    worksheet = writer.sheets[f'Sheet{i}']
+    worksheet = writer.sheets[f'Horario {i+1}']
     
     # Set the column width.
     worksheet.set_column('A:A', 11.11)
